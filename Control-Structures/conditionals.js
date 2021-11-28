2efe23e71bddf6fb95d579a0ff5355536ea0eefc
Bernard Lipat-Silva Jandrey <blipat@winchendon.org>
2021-11-28 17:26:18 -0500

    added Control Structures folder and solutions to Programming Exercises 2.6

---
+++ Control-Structures/conditionals.js
@@ -0,0 +1,42 @@
+/*
+var myName = "Bernard";
+console.log("Hello " + myName);
+if (myName.length > 7) {
+    console.log("Wow, you have a REALLY long name!");
+}
+
+if (boolean_expression) {
+    statement;
+}
+
+{ }
+
+var myName = "Bernard";
+console.log("Hello " + myName);
+if (myName.length > 7) {
+    console.log("Wow, you have a REALLY long name!");
+} else {
+    console.log("Your name isn't very long.");
+}
+
+var soupDumpling = false;
+var generalTsosChicken = true;
+var sesameChicken = true;
+if (soupDumpling) {
+    console.log("Great! I'm having soup dumplings!");
+} else if (generalTsosChicken) {
+    console.log("I'm having the General Tso's chicken.");
+} else if (sesameChicken) {
+    console.log("OK, I'll have the sesame chicken.");
+} else {
+    console.log("Well, I guess I'll have fried rice then.");
+}
+*/
+var dogHeight = 17; //cm; dachshund or pekingese
+var catHeight = 24 //cm; 
+var heightComparison = (dogHeight > catHeight);
+console.log(heightComparison);
+
+var x = 10;
+x+=1;
+console.log(x);
---
+++ exercises-2-6.js
@@ -0,0 +1,31 @@
+//Exercise 1
+
+var myName = "Bernard"
+
+//Exercise 1a
+if (myName === "Bernard") {
+    console.log("Hello me!");
+}
+else {
+    console.log("Hello stranger.");
+}
+
+//Exercise 1b
+if (myName === "Bernard") {
+    console.log("Hello me!");
+}
+else if (myName === "Lucas") {
+    console.log("Hello bestie!");
+}
+else if (myName === "Márcia") {
+    console.log("Olá minha sogra!");
+}
+else {
+    console.log("Hello stranger.");
+}
+
+//Exercise 2
+var time = new Date().getHours();
+if (time < 20) {
+    document.getElementById("demo").innerHTML = "Good day";
+}
