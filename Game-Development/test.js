a663dcb1ffc0769a8baea733735e205a5b52aa16
Bernard Lipat-Silva Jandrey <blipat@winchendon.org>
2022-01-04 12:31:34 -0500

    04jan2022

---
+++ Game-Development/test.js
@@ -0,0 +1,9 @@
+var myString = "winter-spring-summer-fall";
+myArray=myString.split("-");
+console.log(myArray);
+myArray.join("-");
+
+var colors = ["red", "green", "blue"];
+
+var boringAnimals = ["Monkey", "Cat", "Fish", "Lizard"];
+boringAnimals.join();
